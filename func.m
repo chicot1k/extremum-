d675486e70ac420ad855094f8a2410db07a212ae
chicot1k <58642288+chicot1k@users.noreply.github.com>
2020-05-09 01:18:32 +0300

    Add files via upload

--- func.m
+++ func.m
@@ -1,4 +1,11 @@
-function F=func(X)
-a=1;
-b=2;
-F= (1./(a.*cos(X)+b.*sin(X)).^2);
+function F=func(X,m)
+global m
+global par T
+F=0;
+d=[3,5,2,-1];
+for i = 1:m
+   previos=F;
+    F=previos+(d(i)-sin(i.*X)).^2
+end
+end
+
